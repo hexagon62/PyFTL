484e69807ecc38af396b89e423eb4b22c5e15897
HeXaGoN <traviswg9@gmail.com>
2023-04-07 19:59:12 -0700

    Docstrings for reader

--- DLL/Python/BindReader.cpp
+++ DLL/Python/BindReader.cpp
@@ -6,14 +6,13 @@ namespace python_bindings
 
 void bindReader(py::module_& module)
 {
-	module
-		.def("state", &Reader::getState, py::return_value_policy::reference)
-		.def("get_poll_delay", &Reader::getPollDelay, py::return_value_policy::reference)
-		.def("set_poll_delay", &Reader::setPollDelay, py::return_value_policy::reference)
-		.def("use_seperate_polling_thread", &Reader::setSeperateThread, py::arg("on") = true)
-		.def("is_polling_thread_separated", &Reader::usingSeperateThread)
-		.def("reload", &Reader::reload)
-		.def("quit", &Reader::quit)
+	module.def("state", &Reader::getState, py::return_value_policy::reference, "Returns the current state of the game")
+		.def("get_poll_delay", &Reader::getPollDelay, py::return_value_policy::reference, "Returns how long PyFTL waits before doing each poll & input")
+		.def("set_poll_delay", &Reader::setPollDelay, py::return_value_policy::reference, "Sets how long PyFTL waits before doing each poll & input")
+		.def("use_seperate_polling_thread", &Reader::setSeperateThread, py::arg("on") = true, "Tells PyFTL to call on_update and do polls in separate threads")
+		.def("is_polling_thread_separated", &Reader::usingSeperateThread, "Checks if on_update and polling are in separate threads")
+		.def("reload", &Reader::reload, "Tells PyFTL to reload your code")
+		.def("quit", &Reader::quit, "Tells PyFTL to quit. Note this doesn't detach the injected DLL and you'll need to do that manually.")
 		;
 
 	py::class_<ValueScopeGuard<bool>>(module, "ScopedBool", "A boolean that will automatically reset its value after leaving the scope it's defined in")
